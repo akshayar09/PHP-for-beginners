1cc31ac616738859f191e6e61928ace616ea9f72
akshayar09 <akshayar0910@gmail.com>
2023-05-11 17:45:22 +0530

    Intro of OOPs

---
+++ 03.OOPs.php/00.Intro.php
@@ -0,0 +1,11 @@
+* Object Oriented Programming.
+* Creating abjects that contain both data and functions.
+* Faster and easier.
+___________________________________________________________________________________________________________________________
+CLASSES AND OBJECTS
+*************************
+* Classes are template for objects.
+* Objects are instances of classes.
+ex:-
+   Class --> car
+   Object --> BMW,AUDI,FORD
